5abac90797480085b50c87eb0c8b59bea29e1e9a
Github2diogo <55063438+Github2diogo@users.noreply.github.com>
2022-06-29 11:31:34 -0300

    Update and rename funcoes.js to principal.js

--- js/principal.js
+++ js/principal.js
@@ -101,26 +101,3 @@ function filtraLista() {
         }
     }
 }
-
-function dicas(busca){
-    
-    var elem = document.getElementById('caixaDeDicas');
-    
-    if(busca[0] == 'p'){
-        elem.innerHTML = 'Dica: para achar um prédio você pode digitar apenas "p" e o número do prédio'
-    } else { 
-        if(busca.search('lab') > -1 && busca.length <= 12){
-            elem.innerHTML = 'Dica: você pode escrever apenas "lab" para achar um laboratório'
-        } else {
-            if(isNaN(busca) == false && busca.length > 1){
-                elem.innerHTML = 'Dica: se estiver buscando um prédio e não um ramal, tente usar "p" antes do número'
-            } else { 
-                if(!busca == true){
-                    elem.innerHTML = '[[][][][][][][]]'
-                } else {
-                    elem.innerHTML = 'Dica: você pode buscar pela sigla do laboratório / seção / departamento'
-                }
-            }
-        }
-    }
-}
